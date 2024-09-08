b140d0556978d3231729eea50f0f063dbaef4f9c
serezk4 <serezadorohin@gmail.com>
2024-09-08 23:26:09 +0300

    updated deploy.sh

--- src/main/java/com/serezk4/server/FcgiServer.java
+++ src/main/java/com/serezk4/server/FcgiServer.java
@@ -4,6 +4,7 @@ import com.serezk4.server.worker.TestWorker;
 
 public class FcgiServer {
     public static void main(String[] args) {
+        System.out.println("loaded");
         new Thread(new TestWorker()).start();
     }
 }
