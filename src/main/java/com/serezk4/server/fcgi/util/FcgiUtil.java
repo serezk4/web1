9f57813ec7c7b18c20f5876defc2c0e090828397
serezk4 <serezadorohin@gmail.com>
2024-09-12 09:33:15 +0300

    fixed bad properties reading

--- src/main/java/com/serezk4/server/fcgi/util/FcgiUtil.java
+++ src/main/java/com/serezk4/server/fcgi/util/FcgiUtil.java
@@ -6,6 +6,7 @@ import java.io.IOException;
 import java.nio.ByteBuffer;
 import java.nio.charset.StandardCharsets;
 import java.util.Properties;
+import java.util.StringTokenizer;
 
 /**
  * Utility class for FastCGI.
@@ -17,6 +18,7 @@ import java.util.Properties;
 public class FcgiUtil {
     /**
      * Reads the request body from the FastCGI input stream.
+     *
      * @return The request body as a string.
      * @throws IOException If an I/O error occurs.
      */
@@ -36,9 +38,27 @@ public class FcgiUtil {
 
     /**
      * Reads the request parameters from the FastCGI request.
+     *
      * @return The request parameters.
      */
     public static Properties readRequestParams() {
-        return FCGIInterface.request.params;
+        final String paramsString = FCGIInterface.request.params.getProperty("REQUEST_URI", "?").split("\\?")[1];
+        return paramsString.isBlank() ? new Properties() : parseParams(paramsString);
+    }
+
+    /**
+     * Parses the parameters string.
+     *
+     * @param paramsString The parameters string.
+     * @return The parameters.
+     */
+    private static Properties parseParams(String paramsString) {
+        StringTokenizer tokenizer = new StringTokenizer(paramsString, "&");
+        Properties params = new Properties();
+        while (tokenizer.hasMoreTokens()) {
+            String[] pair = tokenizer.nextToken().split("=");
+            params.setProperty(pair[0], pair[1]);
+        }
+        return params;
     }
 }
